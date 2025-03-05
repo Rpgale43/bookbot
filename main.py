4548188379ea76c7a4db8749a56f279ea9f96ca7
Rpgale43 <rpgale43@gmail.com>
2025-03-04 21:02:02 -0600

    added report print structure

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from stats import count_words, count_per_char
+from stats import count_words, count_per_char, produce_report_items
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        num_words = count_words(file_contents)
-        print(f"{num_words} words found in the document")
-        char_count = count_per_char(file_contents)
-        print(char_count)
+        report_list, report_word_count = produce_report_items(file_contents)
+        print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {report_word_count} total words
+--------- Character Count -------""")
+        for char in report_list:
+            print(f"{char["char"]}: {char["num"]}")
+        print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,16 @@ def count_per_char(book):
         else:
             char_dict[c] = 1
     return char_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def produce_report_items(book):
+    report_list = []
+    char_count_dict = count_per_char(book)
+    for item in char_count_dict:
+        if item.isalpha():
+            report_list.append({"char": item, "num": char_count_dict[item]})
+    report_word_count = count_words(book)
+    report_list.sort(reverse=True, key=sort_on)
+    return report_list, report_word_count
